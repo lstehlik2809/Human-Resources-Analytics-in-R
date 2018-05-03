5f6742d0ae5e3a59388697a1a1b7ff69707f367c
ludekstehlik <ludek.stehlik@gmail.com>
2018-05-04 00:17:35 +0200

    ...

--- 01_Recruiting/recruiting.R
+++ 01_Recruiting/recruiting.R
@@ -12,3 +12,22 @@ recruitment %>%
   arrange(desc(n))
 
 # Assessing quality of hires through individual sources
+# sales quota
+avg_sales <- recruitment %>%
+  group_by(recruiting_source) %>%
+  summarise(avg_sales_quota_pct = mean(sales_quota_pct)) %>%
+  arrange(desc(avg_sales_quota_pct))
+
+# Attrition Rates
+avg_attrition <- recruitment %>%
+  group_by(recruiting_source) %>%
+  summarise(attrition_rate = mean(attrition)) %>%
+  arrange(attrition_rate)
+
+# Visualizing the sales performance differences
+ggplot(avg_sales, aes(x = recruiting_source, y = avg_sales_quota_pct))+
+  geom_col()
+
+# Visualizing the attrition differences
+ggplot(avg_attrition, aes(x = recruiting_source, y = attrition_rate))+
+  geom_col()
